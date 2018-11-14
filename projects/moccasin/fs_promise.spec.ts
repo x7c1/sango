d51435af55cffed21dfc637d26222f5280434df4
x7c1 <jmtaro+x7c1@gmail.com>
2018-11-14 23:32:27 +0900

    refactor

--- projects/moccasin/fs_promise.spec.ts
+++ projects/moccasin/fs_promise.spec.ts
@@ -3,28 +3,24 @@ import { readdir, readFile, stat } from "./fs_promise"
 
 describe("readdir", () => {
   it("should return file names", async () => {
-    return readdir(__dirname).then(files => {
-      expect(files).to.include("fs_promise.spec.ts")
-    })
+    const f1 = await readdir(__dirname)
+    expect(f1).to.include("fs_promise.spec.ts")
   })
 })
 
 describe("readFile", () => {
   it("should return contents", async () => {
-    return readFile(__filename).then(file => {
-      expect(file).to.include('describe("readFile"')
-    })
+    const f1 = await readFile(__filename)
+    expect(f1).to.include('describe("readFile"')
   })
 })
 
 describe("stat", () => {
   it("should return contents", async () => {
-    const p1 = stat(__filename).then(file => {
-      expect(file.isDirectory()).to.eq(false)
-    })
-    const p2 = stat(__dirname).then(file => {
-      expect(file.isDirectory()).to.eq(true)
-    })
-    return Promise.all([p1, p2])
+    const f1 = await stat(__filename)
+    expect(f1.isDirectory()).to.eq(false)
+
+    const f2 = await stat(__dirname)
+    expect(f2.isDirectory()).to.eq(true)
   })
 })

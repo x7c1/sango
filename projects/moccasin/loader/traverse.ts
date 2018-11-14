9715b2dc2afb0005f91831d807173397a90d55fd
x7c1 <jmtaro+x7c1@gmail.com>
2018-11-14 23:54:32 +0900

    refactor

--- projects/moccasin/loader/traverse.ts
+++ projects/moccasin/loader/traverse.ts
@@ -2,20 +2,20 @@ import { readdir, stat } from "../fs_promise"
 import { FragmentsLoader, fromTexts, fromYamls } from "./loader"
 
 // rf. https://stackoverflow.com/a/46700791
-const notEmpty = <A> (value: A | null | undefined): value is A => {
+const nonEmpty = <A> (value: A | null | undefined): value is A => {
   return value !== null && value !== undefined
 }
 
+const toDirectoryPath = (dir: string) => async (file: string) => {
+  const path = dir + "/" + file
+  const stats = await stat(path)
+  return stats.isDirectory() ? path : null
+}
+
 const directoriesOf = async (dir: string): Promise<string[]> => {
-  const collect = (path: string) => {
-    return stat(path).then(_ => _.isDirectory() ? path : null)
-  }
-  const loadFiles = (dir: string) => {
-    return readdir(dir).then(_ => _.map(_ => dir + "/" + _))
-  }
-  return loadFiles(dir)
-    .then(_ => Promise.all(_.map(collect)))
-    .then(_ => _.filter(notEmpty))
+  const files = await readdir(dir)
+  const dirs = files.map(toDirectoryPath(dir))
+  return (await Promise.all(dirs)).filter(nonEmpty)
 }
 
 export const traverseTexts = (dir: string) => (): Promise<FragmentsLoader> => {

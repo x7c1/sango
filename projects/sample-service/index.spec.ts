e4052489874d6fbd7fe2081ea74505cb46420314
x7c1 <jmtaro+x7c1@gmail.com>
2018-11-14 23:25:45 +0900

    rename

--- projects/sample-service/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai"
-import { greeting } from "./index"
-
-describe("greeting", () => {
-  it("should have greeting", () => {
-    expect(greeting).to.eq("hello, sample!")
-  })
-})

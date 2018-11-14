a7e452012d2fc805982c760fc543d46121e4a0d5
x7c1 <jmtaro+x7c1@gmail.com>
2018-11-14 23:34:25 +0900

    add tests

---
+++ projects/example-petstore/index.spec.ts
@@ -0,0 +1,36 @@
+import { expect } from "chai"
+import { traverseTexts, traverseYamls } from "moccasin/loader/traverse"
+
+const directories = {
+  schemas: "./projects/example-petstore/components/schemas",
+  paths: "./projects/example-petstore/paths",
+}
+
+describe("traverseTexts", () => {
+  it("concat all texts in a given directory", async () => {
+    const loader = await traverseTexts(directories.schemas)()
+    const contents = await loader.loadContents()
+    const schemas = contents.map(_ => _.dump()).join("")
+
+    expect(schemas).to.include("Error:")
+    expect(schemas).to.include("Pet:")
+    expect(schemas).to.include("Pets:")
+  })
+})
+
+describe("traverseYamls", () => {
+  it("concat all yamls in a given directory", async () => {
+    const loader = await traverseYamls(directories.paths)()
+    const contents = await loader.loadContents()
+    const paths = contents.map(_ => _.dump()).join("")
+
+    const m1 = paths.match(/\/pets:/g)!
+    expect(m1.length).to.eq(1)
+
+    const m2 = paths.match(/\/pets\/{petId}/g)!
+    expect(m2.length).to.eq(1)
+
+    const m3 = paths.match(/\/users\/{userId}/g)!
+    expect(m3.length).to.eq(1)
+  })
+})

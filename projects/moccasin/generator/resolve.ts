7a2dea9458e7f4f31f2f8e6a9e8f99b78058461e
x7c1 <jmtaro+x7c1@gmail.com>
2018-11-16 02:30:06 +0900

    add resolve.ts

---
+++ projects/moccasin/generator/resolve.ts
@@ -0,0 +1,40 @@
+import { ResolvedRefsResults, resolveRefs } from "json-refs"
+import { readFileSync } from "fs"
+import { dump, load } from "js-yaml"
+
+const options = {
+  loaderOptions: {
+    processContent: (result: any, callback: any) => {
+      callback(undefined, load(result.text))
+    },
+  },
+  resolveLocalRefs: false,
+}
+
+/**
+ * https://github.com/whitlockjc/json-refs/blob/master/docs/API.md#module_JsonRefs..ResolvedRefDetails
+ */
+const extractError = (results: ResolvedRefsResults) => {
+  const toError = (ref: any) => {
+
+    // $ref: '#/components/...' などは missing エラーから除外する
+    const isOpenApiRef = ref.missing && ref.uri.startsWith("#/")
+    return !isOpenApiRef && ref.error
+  }
+  return Object.keys(results.refs).
+    map(pointer => {
+      return (results as any).refs[pointer]
+    }).
+    map(toError).
+    filter(_ => _)
+}
+
+export const resolveYamlRef = (filePath: string): Promise<string> => {
+  const root = load(readFileSync(filePath).toString())
+  return resolveRefs(root, options).then((results: ResolvedRefsResults) => {
+    const errors = extractError(results)
+    return errors.length > 0 ?
+      Promise.reject(errors) :
+      Promise.resolve(dump(results.resolved))
+  })
+}

59d6d8fc4a5f2665481bbffd73afb87b587745ea
x7c1 <jmtaro+x7c1@gmail.com>
2018-11-18 20:59:27 +0900

    add tests

--- projects/example-petstore/index.spec.ts
+++ projects/example-petstore/index.spec.ts
@@ -1,6 +1,8 @@
 import { expect } from "chai"
+import { load } from "js-yaml"
 import { FileLogger } from "moccasin/logger/FileLogger"
 import { setupTraverser } from "moccasin/loader/traverse"
+import { readFile } from "moccasin/fs_promise"
 
 const { traverseTexts, traverseYamls } = setupTraverser({
   logger: FileLogger,
@@ -33,5 +35,33 @@ describe("traverseYamls", () => {
 
     const m3 = paths.match(/\/users\/{userId}/g)!
     expect(m3.length).to.eq(1)
+  })
+})
+
+describe("generated OpenAPI yaml", () => {
+
+  const read = async () => {
+    // run generator by this line
+    require("./index")
+
+    const path = "./projects/example-petstore/dist/index.gen.yaml"
+    const text = await readFile(path)
+    return load(text)
+  }
+
+  it("should contain paths", async () => {
+    const root = await read()
+    expect(root["paths"]["/pets"]["post"]).to.exist
+    expect(root["paths"]["/pets"]["get"]).to.exist
+    expect(root["paths"]["/users/{userId}"]["get"]).to.exist
+    expect(root["paths"]["/users/{userId}"]["post"]).to.not.exist
+  })
+
+  it("should contain components", async () => {
+    const root = await read()
+    expect(root["components"]["schemas"]["Error"]).to.exist
+    expect(root["components"]["schemas"]["Pets"]).to.exist
+    expect(root["components"]["schemas"]["Pet"]).to.exist
+    expect(root["components"]["schemas"]["User"]).to.exist
   })
 })

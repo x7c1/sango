3f34cde71a4ab75a5da1d4031a6497af5f495af8
x7c1 <jmtaro+x7c1@gmail.com>
2018-11-23 15:59:22 +0900

    remove needless test

--- projects/sango/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai"
-import { hello } from "./"
-
-describe("hello", () => {
-  it("should return hello", () => {
-    const message = hello("sango")
-    expect(message).to.eq("hello, sango!")
-  })
-})
